96c0b7416cb0a2c244ce362dd051c212f7255c3c
Jose Cervantes <cervantes.rodriguez.jose291@gmail.com>
2025-05-17 23:17:02 -0600

    Actualizacion BFS

--- Pruebas/bfs_prueba.py
+++ Pruebas/bfs_prueba.py
@@ -1,9 +1,85 @@
-grid = [
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1],
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0],
-    [1, 0, 0, 1, 0],
+from collections import deque
+
+
+mapa= [
+    [0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
 ]
 
-print(len(grid))
+
+# print(len(mapa[0])) nos indica las columnas 
+# print(len(mapa)) nos indica las filas 
+
+def valid(visitados, x, y):
+
+    filas = len(mapa)
+    columnas = len(mapa[0])
+
+    # validamos que este dentro de una coordenada valida
+    if not (0 <= x < filas and 0 <= y < columnas):
+        return False
+    
+    # que no se haya visitado ya esta coordenada
+    if (x,y) in visitados:
+        return False
+    
+    # para ignorar en caso de que se encuentre un obstaculo
+    if mapa[x][y] == 1:
+        return False
+
+    return True
+
+# definimos los movimientos que podemos hacer derecha, izquierda, arriba y abajo
+movimientos = [(1,0), (-1,0), (0,1), (0,-1)]
+
+# funcion para resolver el algoritmo 
+def resolver_BFS(origen, destino):
+    fifo = deque()  # fifo first in first out, cola indispensable para logar este algoritmo 
+    padre = {origen:None} # diccinario para poder saber de donde viene cada coordenada y reconstruir el camino
+    visitados = set() # un set para poder llevar un registro de las coordenada que ya se visitaron 
+    visitados.add(origen) # añadimos como visitado la coordenada de origen 
+    fifo.append(origen) # añadimos a la fifo la coordenada de origen 
+
+    # se hace de forma iterativa el recorrido para encontrar el camino 
+
+    # mientras la fifo no este vacia
+    while fifo: 
+        actual = fifo.popleft() # sacamos el ultimo elemento ingresado a la fifo
+
+        # cuando ya encontremos el destino 
+        if actual == destino:
+            camino = [] # arreglo para trazar el camino final 
+            nodo = actual 
+            while nodo is not None:
+                camino.append(nodo) # añadimos el nodo actual al camino
+                nodo = padre[nodo] # el valor de key nodo ahora es el nodo actual. Asi sabemos de donde viene cada coordenada
+            camino.reverse()
+
+            return print(camino)
+        
+        cx, cy = actual # definimos las coordenadas x e y 
+
+        for px, py in movimientos:
+
+            nx, ny = cx + px, cy + py # los posibles caminos que puede tomar actualmente
+
+            if valid(visitados, nx, ny): # validamos que el siguiente movimiento sea valido 
+                visitados.add((nx,ny)) # si es valido entonces lo añadimos a visitados 
+                padre[(nx, ny)] = actual  # añadimos la coordenada valida en el diccionario padre para saber de donde vino
+                fifo.append((nx,ny)) # añadimos las coordenadas validas a la fifo
+    
+    return None
+
+origen = (0,0)
+destino = (9,9)
+
+sol = resolver_BFS(origen, destino)
+
--- pages/BFS.py
+++ pages/BFS.py
@@ -1,26 +1,99 @@
 import streamlit as st
 import plotly.graph_objects as go
+from collections import deque
 
 
-st.markdown("<h1 style='text-align: center;'>Algoritmo DFS </h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Algoritmo BFS </h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;'>(Busqueda por Anchura) </h2>", unsafe_allow_html=True)
 
 # --- Definición del tablero ---
-grid = [
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1],
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0],
-    [1, 0, 0, 1, 0],
+mapa = [
+    [0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
 ]
 
 # Colores: 0 → verde, 1 → rojo
 colors = ['#6c9a8b', '#e07a5f']
 
+# --------------- Algoritmo BFS -------------------------
+
+
+def valid(visitados, x, y):
+
+    filas = len(mapa)
+    columnas = len(mapa[0])
+
+    # validamos que este dentro de una coordenada valida
+    if not (0 <= x < filas and 0 <= y < columnas):
+        return False
+    
+    # que no se haya visitado ya esta coordenada
+    if (x,y) in visitados:
+        return False
+    
+    # para ignorar en caso de que se encuentre un obstaculo
+    if mapa[x][y] == 1:
+        return False
+
+    return True
+
+# definimos los movimientos que podemos hacer derecha, izquierda, arriba y abajo
+movimientos = [(1,0), (-1,0), (0,1), (0,-1)]
+
+# funcion para resolver el algoritmo 
+def resolver_BFS(origen, destino):
+    fifo = deque()  # fifo first in first out, cola indispensable para logar este algoritmo 
+    padre = {origen:None} # diccinario para poder saber de donde viene cada coordenada y reconstruir el camino
+    visitados = set() # un set para poder llevar un registro de las coordenada que ya se visitaron 
+    visitados.add(origen) # añadimos como visitado la coordenada de origen 
+    fifo.append(origen) # añadimos a la fifo la coordenada de origen 
+
+    # se hace de forma iterativa el recorrido para encontrar el camino 
+
+    # mientras la fifo no este vacia
+    while fifo: 
+        actual = fifo.popleft() # sacamos el ultimo elemento ingresado a la fifo
+
+        # cuando ya encontremos el destino 
+        if actual == destino:
+            camino = [] # arreglo para trazar el camino final 
+            nodo = actual 
+            while nodo is not None:
+                camino.append(nodo) # añadimos el nodo actual al camino
+                nodo = padre[nodo] # el valor de key nodo ahora es el nodo actual. Asi sabemos de donde viene cada coordenada
+            camino.reverse()
+
+            return camino
+        
+        cx, cy = actual # definimos las coordenadas x e y 
+
+        for px, py in movimientos:
+
+            nx, ny = cx + px, cy + py # los posibles caminos que puede tomar actualmente
+
+            if valid(visitados, nx, ny): # validamos que el siguiente movimiento sea valido 
+                visitados.add((nx,ny)) # si es valido entonces lo añadimos a visitados 
+                padre[(nx, ny)] = actual  # añadimos la coordenada valida en el diccionario padre para saber de donde vino
+                fifo.append((nx,ny)) # añadimos las coordenadas validas a la fifo
+    
+    return None
+
+# ----------------- Fin Algoritmo BFS ---------------------
+
 # --- Funciones de callback ---
 def resolver():
+    origen = (0,0)
+    destino = (9,9)
     # Ruta de ejemplo (puedes reemplazarla con la salida de tu algoritmo BFS)
-    ruta = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (3, 2), (4, 2), (4, 3), (4, 4)]
+    ruta = resolver_BFS(origen, destino)
     st.session_state['ruta'] = ruta
 
 def reiniciar():
@@ -33,7 +106,7 @@ if 'ruta' not in st.session_state:
 
 # --- Creación del tablero con Plotly ---
 fig = go.Figure(data=go.Heatmap(
-    z=grid,
+    z=mapa,
     colorscale=[
         [0.0, colors[0]],
         [0.5, colors[0]],
@@ -63,14 +136,14 @@ fig.update_layout(
     xaxis=dict(
         showgrid=False,
         zeroline=False,
-        tickvals=list(range(5)),
+        tickvals=list(range(len(mapa[0]))),
         fixedrange=True  # Fijar el rango del eje X
     ),
     yaxis=dict(
         showgrid=False,
         zeroline=False,
         autorange='reversed',
-        tickvals=list(range(5)),
+        tickvals=list(range(len(mapa))),
         fixedrange=True  # Fijar el rango del eje Y
     ),
     margin=dict(l=20, r=20, t=20, b=20)
